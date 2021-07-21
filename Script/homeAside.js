536134a8c178c8c5bc45598d62c27d5ed60a0160
Carlose <carlosviniwork@gmail.comm>
2021-07-21 12:26:27 -0300

    Portifolio 0.3

---
+++ Script/homeAside.js
@@ -0,0 +1,23 @@
+
+const buttonCurriculo = document.getElementById("buttonCurriculo");
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefault();
+
+    const sectCurriculo = document.getElementById('sectCurriculo');
+    const sectionsRight = document.getElementById('complementos');
+    const buttonCurriculo = document.getElementById('buttonCurriculo');
+    const asideLeft = document.getElementById('asideLeft');
+    const teste1 = document.getElementById('teste1');
+    const teste2 = document.getElementById('teste2');
+
+    sectCurriculo.classList.toggle('curriculoOffViewer');
+    sectionsRight.classList.toggle('sectionsRight');
+    buttonCurriculo.classList.toggle('btn-curriculo-active');
+    asideLeft.classList.toggle('asideCurriculo')
+    teste2.classList.toggle('sectionRight')
+    teste1.classList.toggle('sectionRight')
+}
+
+buttonCurriculo.addEventListener('click', toggleMenu);
+buttonCurriculo.addEventListener('touchstart', toggleMenu);
