1cf5ab1927a7a28799a2c63fe9d83a27aeee840f
Carlose <carlosviniwork@gmail.comm>
2021-10-21 12:14:07 -0300

    Iniciando alterações no layout da pagina

--- Script/homeAside.js
+++ Script/homeAside.js
@@ -1,5 +1,3 @@
-const buttonCurriculo = document.getElementById("buttonCurriculo");
-
 
 const buttonCurriculoMobile = document.getElementById("buttonCurriculoMobile");
 
@@ -7,24 +5,15 @@ function trasicaoParaCurriculo(event){
     if(event.type === 'touchstart') event.preventDefault();
 
     const sectCurriculo = document.getElementById('sectCurriculo');
-    const sectionsRight = document.getElementById('complementos');
-    const buttonCurriculo = document.getElementById('buttonCurriculo');
     const asideLeft = document.getElementById('asideLeft');
-    const teste1 = document.getElementById('teste1');
-    const teste2 = document.getElementById('teste2');
+
 
     sectCurriculo.classList.toggle('curriculoOffViewer');
-    sectionsRight.classList.toggle('sectionsRight');
-    buttonCurriculo.classList.toggle('btn-curriculo-active');
     asideLeft.classList.toggle('asideCurriculo')
 
-   /*  essas são as barras que formam o desing da aside left quando fechada, seram removidas em breve */
-    teste2.classList.toggle('sectionRight')
-    teste1.classList.toggle('sectionRight')
 }
 
-buttonCurriculo.addEventListener('click', trasicaoParaCurriculo);
-buttonCurriculo.addEventListener('touchstart', trasicaoParaCurriculo);
+
 
 buttonCurriculoMobile.addEventListener('click', trasicaoParaCurriculo);
 buttonCurriculoMobile.addEventListener('touchstart', trasicaoParaCurriculo);
